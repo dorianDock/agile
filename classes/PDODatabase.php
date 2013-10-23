8f46ef7e1bc04d5bb4137f6283f4113aba5f8ee8
maxime_bibos <meitneshi@gmail.com>
2013-10-23 16:11:45 +0200

    modif PDO CONNECT

--- classes/PDODatabase.php
+++ classes/PDODatabase.php
@@ -7,27 +7,13 @@ class PDODatabase{
     const CONNECTION_TYPE = 'mysql';
     //const CONNECTION_TYPE = 'pgsql';
     
-<<<<<<< HEAD
-<<<<<<< HEAD
-    const DEFAULT_SQL_USER = 'u769120773';
-    
-    const DEFAULT_SQL_HOST = '31.170.164.30';
-    
-    const DEFAULT_SQL_PASS = 'isic_2014';
-        
-    const DEFAULT_SQL_DTB = 'agile';
-=======
-    const DEFAULT_SQL_USER = 'mohamed.khalloqi@minesdedouai.fr';
-=======
     const DEFAULT_SQL_USER = 'u769120773_agile';
->>>>>>> 7e081fbd4a23316cd2eeff9c2226687c1caedd44
     
     const DEFAULT_SQL_HOST = 'localhost';
     
     const DEFAULT_SQL_PASS = 'isic_2014';
-    
+        
     const DEFAULT_SQL_DTB = 'u769120773_agile';
->>>>>>> b54a68b35b1062fd657cd592bdeee75b09a9d9cf
 
     public static function getInstance() {
         if (is_null(self::$instance)) {            
